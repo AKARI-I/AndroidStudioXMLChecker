2313e2ffeb0a40f11931a489e6c5d9e2aed367b5
AKARI-I <i.akari.1122@gmail.com>
2018-08-08 02:00:48 +0900

    Add intent for widget

--- app/src/main/java/com/example/iakari/xmltest/MainActivity.kt
+++ app/src/main/java/com/example/iakari/xmltest/MainActivity.kt
@@ -1,30 +1,18 @@
 package com.example.iakari.xmltest
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_widget.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Button
-        button.setOnClickListener{
-            Toast.makeText(this, "This is Button", Toast.LENGTH_SHORT).show()
-        }
-
-        // ToggleButton
-        toggleButton.setOnClickListener {
-            if(toggleButton.isChecked){
-                // Onの場合
-                checkedTextView.setCheckMarkDrawable(R.drawable.sw_on)
-            } else {
-                // Offの場合
-                checkedTextView.setCheckMarkDrawable(R.drawable.sw_off)
-            }
+        send_widget.setOnClickListener{
+            val intent_widget = Intent(this, Widget::class.java)
+            startActivity(intent_widget)
         }
     }
 }
