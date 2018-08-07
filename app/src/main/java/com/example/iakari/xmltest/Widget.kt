bd80fab31411052107b2da7a506d9cee8c6aaa19
AKARI-I <i.akari.1122@gmail.com>
2018-08-08 01:20:55 +0900

    Update widget screen

--- app/src/main/java/com/example/iakari/xmltest/Widget.kt
+++ app/src/main/java/com/example/iakari/xmltest/Widget.kt
@@ -1,12 +1,30 @@
 package com.example.iakari.xmltest
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class Widget : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_widget)
+
+        // Button
+        button.setOnClickListener{
+            Toast.makeText(this, "This is Button", Toast.LENGTH_SHORT).show()
+        }
+
+        // ToggleButton
+        toggleButton.setOnClickListener {
+            if(toggleButton.isChecked){
+                // Onの場合
+                checkedTextView.setCheckMarkDrawable(R.drawable.sw_on)
+            } else {
+                // Offの場合
+                checkedTextView.setCheckMarkDrawable(R.drawable.sw_off)
+            }
+        }
     }
 }
